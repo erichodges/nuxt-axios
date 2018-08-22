112bd7ff07c87108ce2881e0a0e04b92f0b6c5c9
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-08-22 13:24:10 -0700

    initial commit:

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <h1>Index page</h1>
+  </main>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
