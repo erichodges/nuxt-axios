ad880df077af09e68eb7d2ef0a633cce9b9ad52f
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-08-22 15:48:02 -0700

    working on dotenv config

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,15 @@
 </template>
 
 <script>
+require('dotenv').config()
+import axios from 'axios';
 
+export default function( {params} ) {
 
+  return axios.get(`https://sheets.googleapis.com/v4/spreadsheets/1SIfFSp_1In8V_NmIGjVtry-7478OJosu91J_toQT7gs/values/Sheet1?valueRenderOption=FORMATTED_VALUE&key=process.env.key`)
+    .then((response) => {
+      console.log(response);
+    });
 
-export default {
-  
 }
 </script>
